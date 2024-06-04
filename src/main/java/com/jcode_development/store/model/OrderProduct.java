b85dda6bc0ac789aede9842f2767f6dc3271755d
Jackson Ramos <jackson.ramos@maisunifacisa.com.br>
2024-06-04 19:19:22 -0300

    Romoção da referencia do usuario na tabela Order_product e alteração do 'ddl-auto:' para 'updade'

--- src/main/java/com/jcode_development/store/model/OrderProduct.java
+++ src/main/java/com/jcode_development/store/model/OrderProduct.java
@@ -24,10 +24,6 @@ public class OrderProduct {
 	@JoinColumn(name = "product_id")
 	private Product product;
 	
-	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private User user;
-	
 	private Double price;
 	private Integer quantity;
 }
